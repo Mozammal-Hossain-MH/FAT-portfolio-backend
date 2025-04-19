e45600758c0988f47e4bfb9951ef5514ef0fed20
Mozammal <149988258+Mozammal-Hossain-MH@users.noreply.github.com>
2025-04-19 13:31:26 +0600

    xd

---
+++ prisma/migrations/20250419061355_xd/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Facts" (
+    "id" SERIAL NOT NULL,
+    "Icon" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Facts_pkey" PRIMARY KEY ("id")
+);
